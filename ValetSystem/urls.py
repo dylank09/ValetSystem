b93dc7239db0386539be0317d0ed96bbf575c3a8
Dylan <dylank09@gmail.com>
2021-10-12 17:58:11 +0100

    configured url pattern

--- ValetSystem/urls.py
+++ ValetSystem/urls.py
@@ -17,7 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('ValetApp/', include('ValetApp.urls')),
-    # path('ValetApp/poopy', include('ValetApp.urls')),
-    # path('admin/', admin.site.urls),
+    path('', include('ValetApp.urls')),
 ]
