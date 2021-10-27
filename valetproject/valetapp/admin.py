93b4f1eb711ec166eefca34be54c95856900aa8a
Dylan <dylank09@gmail.com>
2021-10-27 15:41:41 +0100

    added membershipType model and edited Customer model

--- valetproject/valetapp/admin.py
+++ valetproject/valetapp/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models.chainstore import ChainStore
 from .models.users.customer import Customer
 from .models.users.staff import Staff
+from .models.booking import Booking
+from .models.valetservice import ValetService
+from .models.users.membershiptype import MembershipType
 admin.site.register(Customer)
 admin.site.register(Staff)
 admin.site.register(ChainStore)
+admin.site.register(Booking)
+admin.site.register(ValetService)
+admin.site.register(MembershipType)
