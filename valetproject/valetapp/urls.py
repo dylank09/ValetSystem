7ec27bf4e2a6971719533d0d04527e19d82a40f1
The fat man 🐷🦄🛡️📈 <oisin.mcnamara@gmail.com>
2021-10-27 17:46:44 +0100

    added comma

--- valetproject/valetapp/urls.py
+++ valetproject/valetapp/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('chainstore/<int:chainstore_id>', views.chainstore_by_id, name='chainstore_by_id'),
     path('register/', views.register, name='register'),
-<<<<<<< HEAD
-    path('bookingscreen/', views.bookingscreen, name= 'bookingscreen')
-=======
+    path('bookingscreen/', views.bookingscreen, name= 'bookingscreen'),
     path('home/', views.home, name='home'),
->>>>>>> 9b4eedd73754c8d637bad2d2534901685564f1fa
 ]
