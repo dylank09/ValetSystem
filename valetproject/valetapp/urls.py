07ac5b5c02837a6dad0761848c5cd1dacdff9b14
Arnas12345 <arnasjuravicius12345@gmail.com>
2021-10-28 12:27:31 +0100

    merge

--- valetproject/valetapp/urls.py
+++ valetproject/valetapp/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('chainstore/<int:chainstore_id>', views.chainstore_by_id, name='chainstore_by_id'),
+    path('chainstore/<int:chainstore_id>',
+         views.chainstore_by_id, name='chainstore_by_id'),
     path('register/', views.register, name='register'),
-<<<<<<< HEAD
-    path('homescreen/', views.homescreen, name='homescreen')
-=======
-    path('bookingscreen/', views.bookingscreen, name= 'bookingscreen'),
+    path('homescreen/', views.homescreen, name='homescreen'),
+    path('bookingscreen/', views.bookingscreen, name='bookingscreen'),
     path('home/', views.home, name='home'),
->>>>>>> 7ec27bf4e2a6971719533d0d04527e19d82a40f1
 ]
