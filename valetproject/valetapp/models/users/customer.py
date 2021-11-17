4d1ff607dbcb5cabeed175e24aa6f384088652c5
Dylan <dylank09@gmail.com>
2021-11-17 15:10:47 +0000

    separated the views file

--- valetproject/valetapp/models/users/customer.py
+++ valetproject/valetapp/models/users/customer.py
@@ -1,24 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
 from django.dispatch import receiver
-from abc import ABC, abstractmethod
 
+from .observer import Observer
 from .membershiptype import MembershipType
-# from ..booking import Subject
 
-# from .observer import Observer
-
-# class Customer(models.Model, Observer):
     
-class Customer(models.Model):
+class Customer(models.Model, Observer):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     membershipType = models.ForeignKey(
         MembershipType, on_delete=models.RESTRICT, null=True)
 
     def update(self, subject):
         if subject.getPrice() > 0:
-            print("Customer Observer reacted to the event")
 
             if self.membershipType.colour == "gold":
                 subject.setPrice(subject.getPrice()*0.7)
--- valetproject/valetapp/urls.py
+++ valetproject/valetapp/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import views
-from .views.views import BookingList
+from .views import auth
+from .views import booking
+from .views.booking import BookingList
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('bookingservice_form/', views.bookingCreate, name='BookingView'),
+    path('', auth.register, name='index'),
+    path('bookingservice_form/', booking.bookingCreate, name='BookingView'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
     path('payForBooking/<int:bookingId>',
-         views.payForBooking, name='payForBooking'),
-    path('register/', views.register, name='register'),
+         booking.payForBooking, name='payForBooking'),
+    path('register/', auth.register, name='register'),
     path('home/', views.home, name='home'),
-    path('login/', views.loginPage, name='loginUser')
+    path('login/', auth.loginPage, name='loginUser')
 ]
